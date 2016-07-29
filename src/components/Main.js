b5e5d7e7bb394d03ac5a37f765f8ec1fd1856b72
William Sun <williamjinsun@gmail.com>
2016-07-28 20:29:02 -0700

    Item Catalog in progress

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
 require('normalize.css/normalize.css');
 require('styles/App.css');
+require('lodash');
 
 import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
@@ -7,7 +8,7 @@ import React from 'react';
 class Catalog extends React.Component {
     render() {
         return (
-            <table className="table table-inverse">
+            <table className='table'>
 	      		<thead>
 				    <tr>
 				      <th>#</th>
@@ -17,7 +18,8 @@ class Catalog extends React.Component {
 				    </tr>
 	  			</thead>
 	  			<tbody>
-				    <ItemRow itemVals={itemList[0]}/>
+	  			<ItemProfile vals={itemList[0]}/>
+	  				<ItemRow itemVals={itemList[0]}/>
 				    <ItemRow itemVals={itemList[1]}/>
 		   		</tbody>
       		</table>
@@ -25,15 +27,32 @@ class Catalog extends React.Component {
     }
 }
 
-var itemList = [{ "item": "Halibut", "type": "Farm Fresh", "price": 12, 'description':"Everyone's favorite white fish." },
-    { "item": "Wild Alaskan Salmon", "type": "Ocean Catch", "price": 27, 'description':'Big, juicy and shipped quickly' }
+var itemList = [{ 'name': 'Halibut', 'type': 'Farm Fresh', 'price': 12, 'description': 'Everyones favorite white fish.' },
+    { 'name': 'Wild Alaskan Salmon', 'type': 'Ocean Catch', 'price': 27, 'description': 'Big, juicy and shipped quickly' }
 ];
+
+class ItemProfile extends React.Component {
+    render() {
+        return <div>
+        	<div className="col-md-4">
+        		<img className="img-fluid" src="Prof.jpg"/>
+        	</div>
+			<div className="row col-md-8 middle">
+	        	<h1 className="col-md-4">{this.props.vals.name}</h1>
+	        	<h5 className="col-md-4 pull-xs-right">{'$'+this.props.vals.price}</h5>
+			</div>
+        	<h4>{this.props.vals.description}</h4>
+        	<button className="btn btn-default">Add To Order</button>
+        	<div className="clearfix"></div>
+        </div>
+    }
+}
 
 class ItemRow extends React.Component {
     render() {
         return <tr>
-	      <th scope="row">1</th>
-	      <td>{this.props.itemVals.item}</td>
+	      <th scope='row'>1</th>
+	      <td>{this.props.itemVals.name}</td>
 	      <td>{this.props.itemVals.type}</td>
 	      <td>{'$'+this.props.itemVals.price}</td>
 	    </tr>;
