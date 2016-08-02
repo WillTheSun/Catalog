8223620a8d34c65f27ecd6e03a8821d0fb4ac9ae
William Sun <williamjinsun@gmail.com>
2016-08-01 18:18:14 -0700

    changed name?

--- src/components/store.js
+++ src/components/store.js
@@ -32,7 +32,7 @@ class FishList extends React.Component {
     render() {
         this.createFishList();
         return <ul className="list-group">
-          	{this.FishComponents}
+            {this.FishComponents}
         </ul>
     }
 }
@@ -48,25 +48,26 @@ var Header = React.createClass({
           </span>
           Day</h1>
         <h3 className="tagline"><span>Fresh Seafood Market</span></h3>
-      </header>
+        </header>
         )
     }
 })
 
 class Fishbox extends React.Component {
     convertPrice = (x) => {
-        return '$' + x.substring(0, x.length - 2) + '.' + x.substring(x.length - 2, ) }
+        return '$' + x.substring(0, x.length - 2) + '.' + x.substring(x.length - 2, )
+    }
     render() {
         return <li className="list-group-item col-lg-12 col-sm-12 col-xs-12">
-			<div className="col-xs-6 col-sm-4 col-md-4 col-lg-5">
-				<img className="img-fluid" style={{'paddingTop':'20px'}} src={this.props.vals.image}/>
-				<button className="btn btn-danger" style={{'marginTop':'20px', 'width': '100%'}}>Order</button>
-			</div>
-			<div className="col-xs-6 col-sm-8 col-md-7">
-				<h1 className="col-xs-9 col-md-9">{this.props.vals.name}</h1>
-				<h5 className="col-xs-3 col-md-3 pull-xs-right" style={{'paddingTop': '26px'}}>{this.convertPrice(this.props.vals.price.toString())}</h5>	        	
-				<h5 className="col-xs-12">{this.props.vals.desc}</h5>
-			</div>
+            <div className="col-xs-6 col-sm-4 col-md-4 col-lg-5">
+                <img className="img-fluid" style={{'paddingTop':'20px'}} src={this.props.vals.image}/>
+                <button className="btn btn-danger" style={{'marginTop':'20px', 'width': '100%'}}>Order</button>
+            </div>
+            <div className="col-xs-6 col-sm-8 col-md-7">
+                <h1 className="col-xs-9 col-md-9">{this.props.vals.name}</h1>
+                <h5 className="col-xs-3 col-md-3 pull-xs-right" style={{'paddingTop': '26px'}}>{this.convertPrice(this.props.vals.price.toString())}</h5>               
+                <h5 className="col-xs-12">{this.props.vals.desc}</h5>
+            </div>
         </li>
     }
 }
