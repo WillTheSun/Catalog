fc8d5c78ab7dd9bb73eb0a807cbf35d8fd2cd763
William Sun <williamjinsun@gmail.com>
2016-08-02 21:52:11 -0700

    Centralized Data in FishUtils.js, Reoganized component hiearchy, removed unnecessary dependencies.

--- src/components/FishUtils.js
+++ src/components/FishUtils.js
@@ -1,42 +1,34 @@
-require('normalize.css/normalize.css');
-require('styles/App.css');
+require('jquery');
 
-import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
+import lodash from 'lodash';
 
-class FishList extends React.Component {
-    loadFishes() {
-        var jsonList = require('./sample-fishes');
-        this.FishList = [];
-        for (var key in jsonList) {
-            this.FishList.push(jsonList[key]);
-        }
-    }
-    render() {
-        this.loadFishes();
-        return <ul className="list-group">
-            {this.FishList.map(x => <Fishbox vals={x} key={x.name}/> )}
-        </ul>
-    }
+export let FishList = [];
+
+function Fish(name, price, desc, image) {
+    let convertPrice = (x) => {return '$' + x.substring(0, x.length - 2) + '.' + x.substring(x.length - 2)};
+    this.name = name;
+    this.price = convertPrice(price.toString());
+    this.desc = desc;
+    this.image = image;
+    this.status = status;
+    this.quantity = 0;
 }
 
-class Fishbox extends React.Component {
-    convertPrice = (x) => {
-        return '$' + x.substring(0, x.length - 2) + '.' + x.substring(x.length - 2, )
-    }
-    render() {
-        return <li className="list-group-item col-lg-12 col-sm-12 col-xs-12">
-            <div className="col-xs-6 col-sm-4 col-md-4 col-lg-5">
-                <img className="img-fluid" style={{'paddingTop':'20px'}} src={this.props.vals.image}/>
-                <button className="btn btn-danger" style={{'marginTop':'20px', 'width': '100%'}}>Order</button>
-            </div>
-            <div className="col-xs-6 col-sm-8 col-md-7">
-                <h1 className="col-xs-9 col-md-9">{this.props.vals.name}</h1>
-                <h5 className="col-xs-3 col-md-3 pull-xs-right" style={{'paddingTop': '26px'}}>{this.convertPrice(this.props.vals.price.toString())}</h5>
-                <h5 className="col-xs-12">{this.props.vals.desc}</h5>
-            </div>
-        </li>
-    }
+export const addFish = (name, price, desc, image) => {
+    FishList.push(new Fish(name, price, desc, image));
 }
 
-export default FishList;
+export const removeFish = (name) => {
+    FishList = _.reject(FishList,
+        x = (y) => { y.name === name }
+    );
+}
+
+export const loadSamples = () => {
+    var jsonList = require('./sample-fishes');
+    for (var key in jsonList) {
+        var x = jsonList[key];
+        addFish(x.name, x.price, x.desc, x.image);
+    }
+}
--- src/components/Store.js
+++ src/components/Store.js
@@ -1,8 +1,4 @@
-require('normalize.css/normalize.css');
-require('styles/App.css');
-require('jquery');
-
-import FishList from './FishList';
+import { FishList, loadSamples } from './FishUtils';
 import Header from './Header';
 import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
@@ -11,10 +7,29 @@ class Store extends React.Component {
     render() {
         return (
             <div className="col-sm-12 col-md-7 col-lg-5">
-              <Header text="Catch of the Day" text2="Fresh Seafood Market"/>
-              <FishList />
+                <Header text="Catch of the Day" text2="Fresh Seafood Market"/>
+                {loadSamples()}
+            <ul className="list-group">
+                {FishList.map(x => <Fishbox vals={x} key={x.name}/> )}
+            </ul>
             </div>
         );
+    }
+}
+
+class Fishbox extends React.Component {
+    render() {
+        return <li className="list-group-item col-lg-12 col-sm-12 col-xs-12">
+            <div className="col-xs-6 col-sm-4 col-md-4 col-lg-5">
+                <img className="img-fluid" style={{'paddingTop':'20px'}} src={this.props.vals.image}/>
+                <button className="btn btn-danger" style={{'marginTop':'20px', 'width': '100%'}}>Order</button>
+            </div>
+            <div className="col-xs-6 col-sm-8 col-md-7">
+                <h1 className="col-xs-9 col-md-9">{this.props.vals.name}</h1>
+                <h5 className="col-xs-3 col-md-3 pull-xs-right" style={{'paddingTop': '26px'}}>{this.props.vals.price}</h5>
+                <h5 className="col-xs-12">{this.props.vals.desc}</h5>
+            </div>
+        </li>
     }
 }
 
